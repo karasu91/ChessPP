7b26e2750ba5915196b60b99ca5511f8b18fd7c5
karasugg <iiropelli@gmail.com>
2022-07-04 20:23:00 +0300

    add makefile

--- chess/src/cli/client.cpp
+++ chess/src/cli/client.cpp
@@ -1,18 +1,49 @@
 #include "../srv/net.cpp"
+#include "../lib/wt/src/Wt/WApplication.h"
+#include "../lib/wt/src/Wt/WBreak.h"
+#include "../lib/wt/src/Wt/WContainerWidget.h"
+#include "../lib/wt/src/Wt/WLineEdit.h"
+#include "../lib/wt/src/Wt/WPushButton.h"
+#include "../lib/wt/src/Wt/WText.h"
+
+#include <Wt/WApplication>
+
 #include <iostream>
 #include <thread>
 #include <chrono>
 
-int main() {
+class HelloApplication : public Wt::WApplication
+{
+	public:
+		HelloApplication(const Wt::WEnvironment &env);
 
-	Client* client = new Client();
-	client->targetPort = 59203;
-	client->connectServer("192.168.1.103");
+	private:
+		Wt::WLineEdit *nameEdit_;
+		Wt::WText *greeting_;
+};
 
-	while (true) {
-		auto ret = client->sendData("kikkare");
-		if (ret == true)
-			break;
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	}
+
+
+
+HelloApplication::HelloApplication(const Wt::WEnvironment& env)
+    : Wt::WApplication(env)
+{
+    setTitle("Hello world");
+
+    root()->addWidget(std::make_unique<Wt::WText>("Your name, please? "));
+    nameEdit_ = root()->addWidget(std::make_unique<Wt::WLineEdit>());
+    Wt::WPushButton *button = root()->addWidget(std::make_unique<Wt::WPushButton>("Greet me."));
+    root()->addWidget(std::make_unique<Wt::WBreak>());
+    greeting_ = root()->addWidget(std::make_unique<Wt::WText>());
+    auto greet = [this]{
+      greeting_->setText("Hello there, " + nameEdit_->text());
+    };
+    button->clicked().connect(greet);
+}
+
+int main(int argc, char **argv)
+{
+    return Wt::WRun(argc, argv, [](const Wt::WEnvironment& env) {
+      return std::make_unique<HelloApplication>(env);
+    });
 }
